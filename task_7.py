74fc3c795cea40a473fbaea5137553b1c631c9ee
yaa-its-kii <kiyshor25@gmail.com>
2024-04-18 19:57:00 +0530

    task completed

---
+++ task_7.py
@@ -0,0 +1,10 @@
+K = input("Enter a word :")
+G = K.split()  
+F= G[::-1]
+print(' '.join(F))
+reverse=input()
+for i in reverse:
+    if reverse==0:
+        print( reverse)
+    else:
+        print(reverse[::-1])
